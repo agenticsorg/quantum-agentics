51b7c0ab39d6b1dcbbdb2b833fc5a96e1d90b784
rUv <ruv@ruv.net>
2025-02-17 03:17:28 +0000

    Remove hello_world-null agent: delete all associated files and configurations

--- agents/hello_world-null/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-CrewAI Multi-Model Demo using OpenRouter
-"""
-
-__version__ = "0.1.0"
--- agents/hello_world-null/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Change to the project root directory and run the agent
-cd ../.. && python -m agents.hello_world.main "$@"
